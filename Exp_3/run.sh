ce3c035db68b74935635a35e080d492c3d8ba18f
yanpan <cnyanpan@gmail.com>
2019-05-28 09:42:44 +0800

    Add shell to make, ins and rm mod

---
+++ Exp_3/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make
+cd call_os4_module/
+make 
+cd ..
+sudo insmod os4_module.ko 
+sudo insmod call_os4_module/call_os4_module_add_time.ko
+sudo rmmod call_os4_module_add_time
+sudo rmmod os4_module
